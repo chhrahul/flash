07cefc75cca45cc3e016ea7bd04a4e3dacddd4a7
RahulChhabra <chh_rahul@live.com>
2018-10-10 16:11:42 +0530

    2

--- js/openpdf.js
+++ js/openpdf.js
@@ -66,7 +66,7 @@ function openFile(filePath, fileMIMEType) {
  
  function openmypdf(f)
  {
-    cordova.InAppBrowser.open("http://softweavertech.com/projects/visicoil/pdf/"+filename,'_blank', 'location=no');
+    cordova.InAppBrowser.open("http://softweavertech.com/projects/visicoil/pdf/"+f,'_blank', 'location=no');
  }
 
 
